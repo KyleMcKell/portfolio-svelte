057add6a9fb035e26ae0ea740c3cac045c341e3b
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2022-02-21 17:19:59 -0700

    added an excalamation

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -22,7 +22,7 @@
 			<a href="https://kit.svelte.dev/"> SvelteKit</a>.
 		</p>
 		<p>
-			I also LOVE writing <span class="pink"> NIFTY </span>CSS.
+			I also LOVE writing <span class="pink"> NIFTY </span>CSS!
 		</p>
 		<p>☕ Let's chat sometime 💙</p>
 	</article>
