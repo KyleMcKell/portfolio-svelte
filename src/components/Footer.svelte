569bfa3142843d11372ee7f21ed36860de6af157
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2022-02-23 00:29:22 -0700

    added mozilla public license 2.0 as license

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -4,13 +4,17 @@
 
 <footer>
 	<Contact />
+	<p>© Copyright Kyle McKell 2022</p>
 </footer>
 
 <style>
 	footer {
 		background-color: var(--bg-secondary);
-		padding: 4rem 3rem;
+		padding: 4rem 3rem 2rem;
 		display: flex;
 		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		gap: 4rem;
 	}
 </style>
