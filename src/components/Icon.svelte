de879b92682e8e74aa3eb84e7aa0b4bd0b3c3d6c
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2022-02-23 04:26:31 -0700

    added an Icon component

---
+++ src/components/Icon.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { CoffeeIcon, ExternalLinkIcon } from 'svelte-feather-icons';
+	import About from './About.svelte';
+
+	type IconIDs = 'coffee' | 'externalLink';
+
+	export let id: IconIDs;
+	export let color: string = 'currentColor';
+	export let size: string | number = 24;
+	export let strokeWidth: string | number = 2;
+	export let fill: string = 'none';
+
+	const icons: { [key in IconIDs]: any } = {
+		coffee: CoffeeIcon,
+		externalLink: ExternalLinkIcon,
+	};
+
+	$: icon = icons[id];
+	$: if (icon) {
+		if (size) icon.size = size;
+		if (strokeWidth) icon.strokeWidth = strokeWidth;
+		if (fill) icon.fill = fill;
+	} else {
+		throw new Error(`No icon found for ID: ${id}`);
+	}
+	$: Component = icon;
+</script>
+
+<div style={`color: ${color}`} {...$$restProps}>
+	<Component {size} {strokeWidth} {fill} />
+</div>
+
+<style>
+	div {
+		display: inline-block;
+	}
+</style>
